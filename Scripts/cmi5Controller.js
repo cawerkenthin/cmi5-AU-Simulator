5f69128d5c5bf0a1cf846a64ae5b917068274b3b
Art Werkenthin <artw@risc-inc.com>
2019-02-11 10:55:40 -0600

    Begin moving functions from wrapper to controller per 2/8/18
    Created public properties for State items.  Created startUp() method to handle cmi5 initialization tasks (get auth-token, get state. etc).

--- Scripts/cmi5Controller.js
+++ Scripts/cmi5Controller.js
@@ -6,6 +6,10 @@
     var jq = $;         // Local copy of jquery ($)
     var stmt_;
     var Agent_;
+    var endPointConfig;
+    var contextActivities;
+    var contextExtensions;
+    var contextTemplate;
      
     // **********************
     // Public properties    
@@ -16,6 +20,17 @@
     this.activityId = "";
     this.actor = "";
     this.authToken = "";
+     
+    // State parameters (NEW)
+    this.sessionId = "";
+    this.masteryScore = 0;
+    this.launchMode = "";
+    this.launchMethod = "";
+    this.publisherId = "";
+    this.moveOn = "";
+    this.launchParameters = "";
+    this.returnURL = "";
+    this.entitlementKey = { "courseStructure": "", "alternate": "" };
 
     // **********************
     // Private functions
@@ -32,37 +47,83 @@
         s.generateId();
         return s;
     }
+     
+    function SetConfig() {  
+        // Set LRS endpoint configuration
+        endPointConfig = {
+            "endpoint": cmi5Controller.endPoint,
+            "auth": "Basic " + cmi5Controller.authToken
+        };
+    }
+
+    function setStateDocument(r) { 
+        
+        // This is the callback method referenced in call to cmi5Controller.getStateDocument()
+        var obj = JSON.parse(r.response);
+    
+        // Get context activities
+        contextTemplate = obj.contextTemplate;
+        contextActivities = obj.contextTemplate.contextActivities;
+
+        // Get context extensions
+        contextExtensions = obj.contextTemplate.extensions;
+
+        // Get returnUrl
+        var t = typeof (obj["returnURL"]);
+        if (t === "string") {
+            cmi5Controller.returnURL = obj["returnURL"];
+        }
+
+        // Get other state properties
+        cmi5Controller.moveOn = obj["moveOn"];
+        cmi5Controller.masteryScore = obj["masteryScore"];
+        cmi5Controller.launchMode = obj["launchMode"];
+        cmi5Controller.launchMethod = obj["launchMethod"];
+        cmi5Controller.sessionId = contextExtensions["https://w3id.org/xapi/cmi5/context/extensions/sessionid"];
+        cmi5Controller.publisherId = contextActivities.grouping[0].id;
+        cmi5Controller.launchParameters = obj["launchParameters"];
+        cmi5Controller.entities = obj["entitlementKey"];
+    }
 
     // **********************
     // Public functions
     // **********************
     return {
-
+        // cmi5 controller initialization
+        startUp: function(callBack, errorCallBack) {                        
+            cmi5Controller.getAuthToken(errorCallBack);
+            Agent_ = new ADL.XAPIStatement.Agent(cmi5Controller.actor);
+            cmi5Controller.getStateDocument(setStateDocument);
+            // more to come...
+        },
         // getAuthToken calls the fetch url to get the authorization token
-        getAuthToken: function (callBack, tokenErrorCallBack) {
+        getAuthToken: function (tokenErrorCallBack) {
             jq.support.cors = true;
             jq.ajax({
                 async: true,
-                url: fetchUrl,
+                url: cmi5Controller.fetchUrl,
                 type: "POST",
                 dataType: "json"
             })
             .done(function (data) {
                 // Check for error
                 var e = typeof (data["error-code"]);
-                if (e == "string") {
+                if (e === "string") {
                     console.log("getAuthToken: error-code " + data["error-code"] + ": " + data["error-text"]);
-                    callBack("");
+                    if (tokenErrorCallBack && typeof tokenErrorCallBack === "function") { 
+                        tokenErrorCallBack("");                                           
+                    }
                 }
                 e = typeof (data["auth-token"]);
                 if (e == "string") {
-                    callBack(data["auth-token"]);
-
+                    cmi5Controller.authToken = data["auth-token"];        
+                    SetConfig(data["auth-token"]);                        
                 } else {
                     console.log("getAuthToken: Invalid structure returned: " + data.toString());
-                    callBack("");
+                    if (tokenErrorCallBack && typeof tokenErrorCallBack === "function") {
+                        tokenErrorCallBack("");
+                    }
                 }
-
             })
             .fail(function (jqXHR, textStatus, errorThrown) {
                 tokenErrorCallBack(errorThrown);
@@ -76,7 +137,7 @@
         },
         getStateDocument: function (endPointConfig_, activityId_, agent_, registration_, callback_) {
             ADL.XAPIWrapper.changeConfig(endPointConfig_);
-            ADL.XAPIWrapper.getState(activityId_, jq.parseJSON(agent_), "LMS.LaunchData", registration_, null, callback_);
+            ADL.XAPIWrapper.getState(cmi5Controller.activityId, Agent_, "LMS.LaunchData", cmi5Controller.registration, null, callback_);
         },
         getcmi5AllowedStatement: function (agent_, verb_, object_, registration_, contextActivities_, contextExtensions_) {
             stmt_ = GetBasicStatement(agent_, verb_, object_);
@@ -121,7 +182,7 @@
             return stmt_;
         },
         sendStatement: function (endPointConfig_, statement_, callback_) {
-            ADL.XAPIWrapper.changeConfig(endPointConfig_);
+            ADL.XAPIWrapper.changeConfig(endPointConfig);
             ADL.XAPIWrapper.sendStatement(statement_, callback_);
         }
     };
