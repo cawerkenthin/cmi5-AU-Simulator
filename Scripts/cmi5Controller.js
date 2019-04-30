4e1eb224ea6abceab0ff9cbb28274fe0f96f28c4
Art Werkenthin <artw@risc-inc.com>
2019-04-30 12:13:41 -0500

    Track statements sent in array
    1) If an variable is defined called "statementsSent", push all statements sent to that array.
    2) Remove lastVerbSent property as it is a) redundant and b) not accurate when many statements are sent in a row.

--- Scripts/cmi5Controller.js
+++ Scripts/cmi5Controller.js
@@ -1,4 +1,4 @@
-﻿var cmi5Controller = (function () {
+var cmi5Controller = (function () {
 
     // **********************
     // Private variables
@@ -11,10 +11,9 @@
     var contextExtensions;
     var contextTemplate;
     var initializedCallback;
-    var lastVerbSent;
-     
+
     // **********************
-    // Public properties    
+    // Public properties     
     // **********************
     this.endPoint = "";
     this.fetchUrl = "";
@@ -23,8 +22,8 @@
     this.actor = "";
     this.authToken = "";
     this.object = "";
-     
-    // State parameters
+    
+    // State parameters 
     this.sessionId = "";
     this.masteryScore = 0;
     this.launchMode = "";
@@ -50,11 +49,11 @@
         s.generateId();
         return s;
     }
-     
+
     function AuthTokenFetched() {  
         cmi5Controller.getStateDocument(setStateDocument);
-    } 
-     
+    }
+
     function SetConfig() {  
         // Set LRS endpoint configuration
         endPointConfig = {
@@ -62,7 +61,7 @@
             "auth": "Basic " + cmi5Controller.authToken
         };
     }
-     
+
     function setAgentProfile(r) {                                                              
         // This is the callback method referenced in call to cmi5Controller.getAgentProfile()  
         if (r.response) {
@@ -78,7 +77,7 @@
         }
         initializedCallback();
     }
- 
+
     function setStateDocument(r) {
         if (r.response) {
 
@@ -107,21 +106,24 @@
             cmi5Controller.publisherId = contextActivities.grouping[0].id;
             cmi5Controller.launchParameters = obj["launchParameters"];
             cmi5Controller.entitlementKey = obj["entitlementKey"];
-            
-            // Now get agent profile
+
             cmi5Controller.getAgentProfile(setAgentProfile);
         } else {
             console.log("No state document found");
         }
     }
-     
+
     // **********************
     // Public functions
     // **********************
     return {
         // cmi5 controller initialization
-        startUp: function(callBack, errorCallBack) {                        
+        startUp: function(callBack, errorCallBack) {
             initializedCallback = callBack;
+            cmi5Controller.object = {
+                "objectType": "Activity",
+                "id": cmi5Controller.activityId
+            };
             cmi5Controller.getAuthToken(AuthTokenFetched, errorCallBack);
         },
         getAUActivityId: function() {               
@@ -131,17 +133,15 @@
             return cmi5Controller.returnURL;
         },
         getContextActivities() {
-            return contextActivities;
+            return contextActivities; 
         },
         getContextExtensions() {
-            return contextExtensions;
-        },
-        getLastVerbSent() {                                         
-            return lastVerbSent;
+            return contextExtensions;  
         },
         setEndPoint: function(endpoint) {               
             if (endpoint) {
                 cmi5Controller.endPoint = endpoint;
+                console.log("Endpoint set to " + endpoint);
             } else {
                 console.log("Invalid value passed to setEndpoint()");
             }
@@ -149,6 +149,7 @@
         setFetchUrl: function(fetchUrl) {
             if (fetchUrl) {
                 cmi5Controller.fetchUrl = fetchUrl;
+                console.log("fetchUrl set to " + fetchUrl);
             } else {
                 console.log("Invalid value passed to setFetchUrl()");
             }
@@ -164,13 +165,14 @@
                 cmi5Controller.object.definition.name[language_] = name_;
             }
             if (description_) {
-                cmi5Controller.object.definition.description = {};
+                cmi5Controller.object.definition.description = {};                          
                 cmi5Controller.object.definition.description[language_] = description_;
             }
         },
         setRegistration: function(registration) {
             if (registration) {
                 cmi5Controller.registration = registration;
+                console.log("Registration set to " + registration);
             } else {
                 console.log("Invalid value passed to setRegistration()");
             }
@@ -178,6 +180,7 @@
         setActivityId: function(activityId) {
             if (activityId) {
                 cmi5Controller.activityId = activityId;
+                console.log("Activity ID set to " + activityId);
             } else {
                 console.log("Invalid value passed to setActivityId()");
             }
@@ -206,13 +209,13 @@
             } else {
                 console.log("Invalid value passed to setActor()");
             }
-        },   
+        },                                                                      
         // getAuthToken calls the fetch url to get the authorization token
-        getAuthToken: function (successCallback, tokenErrorCallBack) {
+        getAuthToken: function (successCallback, tokenErrorCallBack) {                    
             jq.support.cors = true;
             jq.ajax({
                 async: true,
-                url: cmi5Controller.fetchUrl,
+                url: cmi5Controller.fetchUrl,                               
                 type: "POST",
                 dataType: "json"
             })
@@ -220,21 +223,22 @@
                 // Check for error
                 var e = typeof (data["error-code"]);
                 if (e === "string") {
-                    console.log("getAuthToken: error-code " + data["error-code"] + ": " + data["error-text"]);
+                    console.log("error-code " + data["error-code"] + ": " + data["error-text"]);    
                     if (tokenErrorCallBack && typeof tokenErrorCallBack === "function") { 
                         tokenErrorCallBack("");                                           
                     }
                 }
+
                 e = typeof (data["auth-token"]);
-                if (e == "string") {
+                if (e === "string") {
                     cmi5Controller.authToken = data["auth-token"];        
-                    SetConfig(data["auth-token"]);
+                    SetConfig(data["auth-token"]);                        
                     if (successCallback && typeof successCallback === "function") { 
                         successCallback();
                     }
                 } else {
-                    console.log("getAuthToken: Invalid structure returned: " + data.toString());
-                    if (tokenErrorCallBack && typeof tokenErrorCallBack === "function") {
+                    console.log("Invalid structure returned: " + data.toString());
+                    if (tokenErrorCallBack && typeof tokenErrorCallBack === "function") { 
                         tokenErrorCallBack("");
                     }
                 }
@@ -248,7 +252,7 @@
             ADL.XAPIWrapper.getAgentProfile(Agent_, "cmi5LearnerPreferences", null, callback);
             return false;
         },
-        getStateDocument: function (callback) {       
+        getStateDocument: function (callback) {      
             ADL.XAPIWrapper.changeConfig(endPointConfig);
             ADL.XAPIWrapper.getState(cmi5Controller.activityId, Agent_, "LMS.LaunchData", cmi5Controller.registration, null, callback);
         },
@@ -258,7 +262,7 @@
             // Add registration
             stmt_.context = {};
             stmt_.context.registration = cmi5Controller.registration;
-            
+
             // If context parms are not passed, use defaults from STATE.                   
             if (!contextActivities_) {
                 contextActivities_ = cmi5Controller.getContextActivities();
@@ -268,9 +272,10 @@
                 contextExtensions_ = cmi5Controller.getContextExtensions();
             }
 
-            // Remove cmi5 category from context activities                    
+            // Remove cmi5 category from context activities                     
             var z = contextActivities_;
             if (z.hasOwnProperty("category")) {
+                // Do not include the cmi5 category for "allowed" statements
                 delete z.category;
             }
             stmt_.context.contextActivities = z;
@@ -281,19 +286,19 @@
             return stmt_;
         },
         getcmi5DefinedStatement: function (verb_, contextExtentions_) {
-            stmt_ = GetBasicStatement(verb_, cmi5Controller.object);      
+            stmt_ = GetBasicStatement(verb_, cmi5Controller.object);   
             
             // If context extensions not passed, use default.
             if (!contextExtentions_) {                                          
                 contextExtentions_ = cmi5Controller.getContextExtensions();
             }
-
+            
             // Add registration
             stmt_.context = {};
             stmt_.context.registration = cmi5Controller.registration;
 
             // Context activities from State API
-            var z = contextActivities_;
+            var z = contextActivities;
             stmt_.context.contextActivities = z;
 
             // cmi5 Context activity
@@ -304,27 +309,25 @@
             });
 
             // Extensions
-            stmt_.context.extensions = contextExtensions_;
+            stmt_.context.extensions = contextExtentions_;
 
             return stmt_;
         },
-        sendAllowedState: function(stateid_, statevalue_, matchHash_, noneMatchHash_, callback_) {        
+        sendAllowedState: function(stateid_, statevalue_, matchHash_, noneMatchHash_, callback_) {         
             ADL.XAPIWrapper.changeConfig(endPointConfig);
             ADL.XAPIWrapper.sendState(cmi5Controller.activityId, stateid_, statevalue_, matchHash_, noneMatchHash_, callback_);
         },
-        getAllowedState: function(stateid_, since_, callback_) {                                         
+        getAllowedState: function(stateid_, since_, callback_) {                                          
             ADL.XAPIWrapper.changeConfig(endPointConfig);
             ADL.XAPIWrapper.getState(cmi5Controller.activityId, Agent_, stateid_, cmi5Controller.registration, since_, callback_);
         },
         sendStatement: function (statement_, callback_) {
-            if (statement_.verb.display["en-US"]) {
-                lastVerbSent = statement_.verb.display["en-US"];
-            } else if (statement_.verb.display["und"]) {
-                lastVerbSent = statement_.verb.display["und"];
-            } else {
-                lastVerbSent = statement_.verb.id;
+            // NEW
+            // If array statementsSent is defined, push statements to the array.
+            if (window.statementsSent && typeof(window.statementsSent) === "object" && Array.isArray(window.statementsSent)) {
+                window.statementsSent.push(statement_);
             }
-            
+
             ADL.XAPIWrapper.changeConfig(endPointConfig);
             ADL.XAPIWrapper.sendStatement(statement_, callback_);
         }
